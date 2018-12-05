326f4e7260b695720fd4cee792db9f16b68a776c
Igor <igor@miniasp.com>
2018-12-05 16:44:10 +0800

    更新使用API CALL 資料

--- src/app/services/realword.service.ts
+++ src/app/services/realword.service.ts
@@ -10,15 +10,17 @@ export class RealwordService {
   constructor(private http: HttpClient) {}
 
   getArticleList() {
-    return this.http.get('http://localhost:3000/article');
+    return this.http.get('http://localhost/RealWorldApi/api/RealWorld');
+    // return this.http.get('http://localhost:3000/article');
   }
 
   getArticleContent(articleid) {
+    // return this.http.get(`QueryCollectionBillApi/TestValidTime/${articleid}`);
+    //return this.http.get(`http://localhost:83/RealWorld/RealWordController${articleid}`);
     return this.http.get(`http://localhost:3000/article/${articleid}`);
   }
 
   createArticle(body){
     this.http.post('http://localhost:3000/article',body);
   }
-
 }
