f78a0b0cd1f8b431ee50859f6c6cd3fa98944b82
Igor <igor@miniasp.com>
2018-12-14 10:10:45 +0800

    LOGIN

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
-
+  loginFrom;
+  token :string;
   content;
   email;
   password;
 
-  constructor() { }
+  constructor(private _loginService: LoginService) { }
 
   ngOnInit() {
+
+  }
+
+  login() {
+    this.loginFrom = { account: this.email, password: this.password, content: this.content }
+    this._loginService.login(this.loginFrom).subscribe(x =>{
+      localStorage.setItem('token',JSON.stringify(x));
+    });
+
   }
 
 }
